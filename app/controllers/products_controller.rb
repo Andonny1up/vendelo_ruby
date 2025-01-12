b15534f41695eb38d2254f2bd7ed1a812f99f65c
Andoni Barba <andoni@andoni.andoni>
2025-01-12 19:30:37 -0400

    first commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
+
+    def show
+        id = params[:id]
+        
+        @product = Product.find(id)
+    end
+end
